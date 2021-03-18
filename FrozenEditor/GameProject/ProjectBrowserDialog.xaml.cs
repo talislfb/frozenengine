158fe489c5dca35b903a24fc908881b5c8c240e6
Talis <talis.lfb@gmail.com>
2021-03-17 23:08:26 -0400

    + Create/Open project basic window working

---
+++ FrozenEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FrozenEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialog.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialog : Window
+    {
+        public ProjectBrowserDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OnToggleButton_Clicked(object Sender, RoutedEventArgs e)
+        {
+            if(Sender == ButtonOpenProject)
+            {
+                if(ButtonCreateProject.IsChecked == true)
+                {
+                    ButtonCreateProject.IsChecked = false;
+                    ProjectBrowserContent.Margin = new Thickness(0);
+                }
+                ButtonOpenProject.IsChecked = true;
+            }
+            else
+            {
+                if(ButtonOpenProject.IsChecked == true)
+                {
+                    ButtonOpenProject.IsChecked = false;
+                    ProjectBrowserContent.Margin = new Thickness(-800, 0, 0, 0);
+                }
+                ButtonCreateProject.IsChecked = true;
+            }
+        }
+    }
+}
---
+++ FrozenEditor/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using FrozenEditor.GameProject;
+
+namespace FrozenEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var ProjectBrowser = new ProjectBrowserDialog();
+
+            // ShowDialog returns a nullable bool, hence the == true
+            if(ProjectBrowser.ShowDialog() == true)
+            {
+
+            } else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
